96a636605d74bee23ca46005c0381bf9c4d2ab0b
VagueSky8812 <95860217+VagueSky8812@users.noreply.github.com>
2025-10-29 17:39:06 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -35,26 +35,40 @@ def send(cmd):
 
 ###########################################################################################################
 def Set_DC_Voltage_PSupply(Source_ID, Vapp):
-    send("INIT")
+    #print(f"selecting channel {Source_ID}")
     # Select channel 1
     send(f"INST:NSEL {Source_ID}")
 
+    #print("Initialising")
+    send("INIT")
+
     # Set voltage and current
+    #print(f'Sending voltage {Vapp}V')
     send(f"VOLT {Vapp}")
+
     voltage_str = send_query('MEAS:VOLT?')
+    #print("Read back Voltage string 1st time")
     while (voltage_str is None):
         voltage_str = send_query('MEAS:VOLT?')
-    voltage = float(voltage_str)
+        #print("Voltage string read back none")
+    #print("Out of none loop 1")
+
+    voltage = float(voltage_str.split("\n", 1)[0])
+    #print(f"Voltage read = {voltage}")
     voltage_target = Vapp
     voltage_rel_err = 1
-    while(voltage_rel_err > 0.05):  #keep measuring voltage till voltage stabilises within 1% error
+    max_iter = 10
+    iter = 0
+    while((voltage_rel_err > 0.05) and (iter <= max_iter)):  #keep measuring voltage till voltage stabilises within 1% error
+        iter += 1
         voltage_str = send_query('MEAS:VOLT?')  #measure 1st
         while(voltage_str is None):             #repeat till we get a non-none value
             voltage_str = send_query('MEAS:VOLT?')
-        voltage = float(voltage_str)
+        voltage = float(voltage_str.split("\n", 1)[0])
+        #print(f"Voltage read = {voltage}")
         if (voltage < 0.01):     #don't do it for 10mV or lower measures
             break
-        voltage_rel_err = abs(1 - voltage_target/voltage)
+        voltage_rel_err = abs(1 - voltage/voltage_target)
         #print(f"Target Voltage = {Vapp}V; Measured Voltage = {voltage}V; Rel Error = {voltage_rel_err}")
     return voltage
     #time.sleep(0.5)
@@ -63,21 +77,20 @@ def Set_DC_Voltage_PSupply(Source_ID, Vapp):
 # Measure Voltage and Current
 def Measure_DC_I(Source_ID = 1):
     # Select channel 1
-    #send("INIT")
-    send(f"INST:NSEL {Source_ID}")
-    #time.sleep(1)
     send("INIT")
+    send(f"INST:NSEL {Source_ID}")
+    
     current_str = send_query('MEAS:CURR?')
     while(current_str is None):
         current_str = send_query('MEAS:CURR?')
-    current = float(current_str)
+    current = float(current_str.split("\n", 1)[0])
     current_prev = current
     current_rel_err = 1
     while(current_rel_err > 0.01):  #keep measuring current till voltage stabilises within 1% error
         current_str = send_query('MEAS:CURR?')  #measure 1st
         while(current_str is None):             #repeat till we get a non-none value
             current_str = send_query('MEAS:CURR?')
-        current = float(current_str)
+        current = float(current_str.split("\n", 1)[0])
         if (current < 0.001):     #don't do it for 1mA or lower measures
             break
         current_rel_err = abs(1 - current_prev/current)
